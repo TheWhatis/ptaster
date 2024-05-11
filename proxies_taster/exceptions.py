b1508c1c4f5ca287d4f25513973c52598bd9f2c0
TheWhatis <anton-gogo@mail.ru>
2024-05-11 13:24:50 +0700

    Перекинул в ptaster

--- proxies_taster/exceptions.py
+++
@@ -1,7 +0,0 @@
-class TooManyOpenFilesError(Exception):
-    def __init__(self, message: str, previous: Exception):
-        super().__init__(message)
-        self.previous = previous
-
-    def getPrevious() -> Exception:
-        return self.previous
